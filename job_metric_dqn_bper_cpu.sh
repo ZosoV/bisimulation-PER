7c5636fc71377371f89c5a6894b81d729de99c14
ZosoV <hoskarg9619@gmail.com>
2025-03-31 13:12:46 +0100

    Setting seed 1 with device a30

--- job_metric_dqn_bper_cpu.sh
+++ job_metric_dqn_bper_cpu.sh
@@ -122,10 +122,13 @@ trap notify_job_completion EXIT
 # Print current OMP_NUM_THREADS and MKL_NUM_THREADS
 echo "OMP_NUM_THREADS=$OMP_NUM_THREADS"
 echo "MKL_NUM_THREADS=$MKL_NUM_THREADS"
+echo "XLA_FLAGS=$XLA_FLAGS"
 
 # # Set the number of threads for MKL and OMP
-export OMP_NUM_THREADS=$CUSTOM_THREADS
-export MKL_NUM_THREADS=$CUSTOM_THREADS
+# export OMP_NUM_THREADS=$CUSTOM_THREADS
+# export MKL_NUM_THREADS=$CUSTOM_THREADS
+# export XLA_FLAGS=--xla_cpu_multi_thread_eigen=true\ --xla_cpu_multi_thread_eigen_num_threads=$CUSTOM_THREADS
+
 
 # Execute based on the selected variant
 if [ "$AGENT_NAME" == "metric_dqn_bper" ]; then
