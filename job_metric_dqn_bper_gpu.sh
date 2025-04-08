b55e88c60857465831a5ddda45c08c824c1dded6
ZosoV <hoskarg9619@gmail.com>
2025-04-08 01:14:00 +0100

    from zero installation

--- job_metric_dqn_bper_gpu.sh
+++ job_metric_dqn_bper_gpu.sh
@@ -84,17 +84,16 @@ PIP_CACHE_DIR="/scratch/${USER}/pip"
 # Perform any required pip installations. For reasons of consistency we would recommend
 # that you define the version of the Python module – this will also ensure that if the
 # module is already installed in the virtual environment it won't be modified.
+python3 -m pip install --no-cache-dir --upgrade pip
 pip install dopamine-rl
-# cd baselines && pip install -e .
-# cd ..
+python3 -m pip install 'tensorflow[and-cuda]'
+cd baselines && pip install -e .
+cd ..
 pip install ale-py
 pip install seaborn
 pip install tqdm
-pip uninstall -y jax jaxlib
-# pip install jax==0.4.9
-# pip install --upgrade "jaxlib==0.4.9+cuda11.cudnn86" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
-# pip install --upgrade "jax[cuda11_pip]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
+# pip uninstall -y jax jaxlib
+# pip install --upgrade "jax[cuda]" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 
 seeds=(118398 919409 711872 442081 189061)
 
